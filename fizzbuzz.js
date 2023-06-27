a0af7b633fd5cbf7064a46d3d2bc209f8d057d4f
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-06-27 13:37:23 +0100

    Custom Rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,31 @@
 // This is our main function
 function fizzbuzz() {
     const prompt = require("prompt-sync")({ sigint: true });
-    let max = 0
-    // get integer input
+    let max
+    let rules
+    let ruleOprs = /[+=\\]/
+
+    // get rules
+    console.log("====== Define Rules ======")
+    console.log(`
+        =fizz means print 'fizz' if a multiple of this number
+        +fizz means append 'fizz' if a multiple of this number
+        \\means reverse the current string
+        
+        give input as format: 3=Fizz,17+Buzz`)
     while(true){
-        console.log("====== Select Max Number ======")
+        rules = prompt()
+
+        if(/^\d+(?:\\|[+=]\w+)(?:,\d+(?:\\|[+=]\w+))*$/.test(rules)){
+            rules = rules.split(",")
+            break
+        } else{
+            console.log("syntax error")
+        }
+    }
+
+    while(true){
+        console.log("====== Select Max Iterations ======")
         max = Number(prompt());
         if(Number.isInteger(max)){
             break
@@ -13,34 +34,31 @@ function fizzbuzz() {
         }
     }
 
-    for (let i = 0; i<max; i++){
+    //TODO: use data structure for rules instead of parsing input rules twice
+    for (let num = 0; num<max; num++){
         let s = ""
-        if(i%3===0) {
-            s = 'Fizz'
-        }
-        if(i%13===0){
-            s += 'Fezz'
-        }
-        if(i%11===0) {
-            if (s === "Fezz") {
-                s += 'Bong'
-            } else {
-                s = 'Bong'
+        let toFlip = false
+
+        //iterate over rules
+        for(let rule of rules){
+            let [mulOf,strToAdd] = rule.split(ruleOprs)
+            if(num%Number(mulOf)==0){
+                if(rule.includes("+")){
+                    s += ' ' + strToAdd
+                } else if(rule.includes("=")){
+                    s = strToAdd
+                } else if (rule.includes("\\")){
+                    toFlip = true
+                }
             }
         }
-        if(i%5===0){
-            s += 'Buzz'
-        }
-        if(i%7===0) {
-            s += 'Bang'
-        }
-        if(i%17===0&s!=""){
-            s = s.split(/(?=[A-Z])/).reverse().join('');
-        }
         if(s===""){
-            console.log(i)
+            console.log(num)
         } else {
-            console.log(s)
+            if(toFlip){
+                console.log(s.split(' ').reverse().join(''))
+            }
+            console.log(s.replace(/\s/g,''))
         }
     }
 
