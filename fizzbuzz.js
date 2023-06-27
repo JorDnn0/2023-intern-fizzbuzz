a7c96b44ff271b5c5d6d5550843a382f3ee47da1
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-06-27 09:54:54 +0100

    all tasks up to custom rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,48 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
+    const prompt = require("prompt-sync")({ sigint: true });
+    let max = 0
+    // get integer input
+    while(true){
+        console.log("====== Select Max Number ======")
+        max = Number(prompt());
+        if(Number.isInteger(max)){
+            break
+        } else{
+            console.log("number is not an int!")
+        }
+    }
 
-    // Put your code here...
+    for (let i = 0; i<max; i++){
+        let s = ""
+        if(i%3===0) {
+            s = 'Fizz'
+        }
+        if(i%13===0){
+            s += 'Fezz'
+        }
+        if(i%11===0) {
+            if (s === "Fezz") {
+                s += 'Bong'
+            } else {
+                s = 'Bong'
+            }
+        }
+        if(i%5===0){
+            s += 'Buzz'
+        }
+        if(i%7===0) {
+            s += 'Bang'
+        }
+        if(i%17===0&s!=""){
+            s = s.split(/(?=[A-Z])/).reverse().join('');
+        }
+        if(s===""){
+            console.log(i)
+        } else {
+            console.log(s)
+        }
+    }
 
 }
 
